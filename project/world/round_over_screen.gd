c2b6dec6077b2edc1f8dfe0664211c95661666d3
Alex Gestwicki <alex.gestwicki@gmail.com>
2024-01-27 12:30:46 -0500

    Put round over scores into a grid

--- project/world/round_over_screen.gd
+++ project/world/round_over_screen.gd
@@ -3,5 +3,5 @@ extends PanelContainer
 
 func update_wins(player_wins:Dictionary)->void:
 	for i in player_wins:
-		get_node("VBoxContainer/Player%sLabel" % i).text = "Chipmunk %s: %s win%s" % \
-		[i, player_wins[i], "" if player_wins[i] == 1 else "s"] 
+		get_node("GridContainer/Player%sLabel" % i).text = "Chipmunk %s:" % i
+		get_node("GridContainer/Player%sScore" % i).text = "%s win%s" % [player_wins[i], "" if player_wins[i] == 1 else "s"]
