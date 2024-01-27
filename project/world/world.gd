9bf461bfd16e068398117be7bab3c64b24ea963c
Paul Gestwicki <pvgestwicki@bsu.edu>
2024-01-27 10:20:14 -0500

    Reload level after the round ends

--- project/world/world.gd
+++ project/world/world.gd
@@ -3,6 +3,8 @@ extends Node2D
 ## The number of players that start in this round of the game
 @export var player_count := 4
 
+@export var seconds_between_rounds := 3.0
+
 @onready var _spawn_point_parent := $SpawnPoints
 
 var _players : Array[Player]
@@ -26,3 +28,8 @@ func _on_player_died(player:Player) -> void:
 	_players.erase(player)
 	if _players.size() == 1:
 		$GameOverLabel.visible = true
+		await get_tree().create_timer(seconds_between_rounds).timeout
+		var new_scene = load("res://world/world.tscn").instantiate()
+		new_scene.player_count = player_count
+		add_sibling(new_scene)
+		queue_free()
