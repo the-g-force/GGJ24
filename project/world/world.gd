2d57ea3d19af3285aef55d77e3745fc6f5d2209c
Alex Gestwicki <alex.gestwicki@gmail.com>
2024-01-27 11:21:32 -0500

    Track player wins

--- project/world/world.gd
+++ project/world/world.gd
@@ -1,16 +1,21 @@
 extends Node2D
 
 ## The number of players that start in this round of the game
-@export var joined_player_ids : Array[int] = [0, 1, 2, 3]
+@export var joined_player_ids : Array[int] = [0, 2]
 @export var seconds_between_rounds := 3.0
 
 @onready var _spawn_point_parent := $SpawnPoints
 
 var _players : Array[Player]
+var player_wins := {}
 
 func _ready():
 	for i in joined_player_ids:
 		_add_player(i)
+	if player_wins.size() < joined_player_ids.size():
+		for id in joined_player_ids:
+			if not player_wins.has(id):
+				player_wins[id] = 0
 
 
 func _add_player(id:int)->void:
@@ -26,9 +31,24 @@ func _add_player(id:int)->void:
 func _on_player_died(player:Player) -> void:
 	_players.erase(player)
 	if _players.size() == 1:
-		$GameOverLabel.visible = true
-		await get_tree().create_timer(seconds_between_rounds).timeout
-		var new_scene = load("res://world/world.tscn").instantiate()
-		new_scene.joined_player_ids = joined_player_ids
-		add_sibling(new_scene)
-		queue_free()
+		var winner_id := _players[0].id
+		player_wins[winner_id] += 1
+		if player_wins[winner_id] == 3:
+			_end_game(winner_id)
+		else:
+			_end_round()
+
+
+func _end_round()->void:
+	$GameOverLabel.visible = true
+	await get_tree().create_timer(seconds_between_rounds).timeout
+	var new_scene = load("res://world/world.tscn").instantiate()
+	new_scene.joined_player_ids = joined_player_ids
+	new_scene.player_wins = player_wins
+	add_sibling(new_scene)
+	queue_free()
+
+
+func _end_game(winner_id:int)->void:
+	$GameOverLabel.visible = true
+	$GameOverLabel.text = "Game Over! Chipmunk %s won! \n The game will never end now" % winner_id
