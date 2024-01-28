e2d1681166c19877c53f67ec2236b97d210b0222
Alex Gestwicki <alex.gestwicki@gmail.com>
2024-01-28 15:13:59 -0500

    Change wording on round over screen

--- project/world/round_over_screen.gd
+++ project/world/round_over_screen.gd
@@ -4,7 +4,7 @@ extends PanelContainer
 func update_wins(player_wins:Dictionary, player_colors:Dictionary)->void:
 	for i in player_wins:
 		var player_label : Label = get_node("VBoxContainer/GridContainer/Player%sLabel" % i)
-		player_label.text = "%s \n\n" % (i + 1)
+		player_label.text = "Player %s \n\n" % (i + 1)
 		var wins_label : Label = get_node("VBoxContainer/GridContainer/Player%sScore" % i)
 		wins_label.text = "%s \n\n" % player_wins[i]
 		
