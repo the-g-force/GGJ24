9d8a0fc67285f2817e90fe55cbddbbe07a36956e
Alex Gestwicki <alex.gestwicki@gmail.com>
2024-01-28 14:57:06 -0500

    Rearrange end round popup

--- project/world/round_over_screen.gd
+++ project/world/round_over_screen.gd
@@ -4,9 +4,9 @@ extends PanelContainer
 func update_wins(player_wins:Dictionary, player_colors:Dictionary)->void:
 	for i in player_wins:
 		var player_label : Label = get_node("VBoxContainer/GridContainer/Player%sLabel" % i)
-		player_label.text = "Player %s: \n\n" % (i + 1)
+		player_label.text = "%s \n\n" % (i + 1)
 		var wins_label : Label = get_node("VBoxContainer/GridContainer/Player%sScore" % i)
-		wins_label.text = "%s win%s \n\n" % [player_wins[i], "" if player_wins[i] == 1 else "s"]
+		wins_label.text = "%s \n\n" % player_wins[i]
 		
 		player_label.label_settings.font_color = player_colors[i]
 		wins_label.label_settings.font_color = player_colors[i]
