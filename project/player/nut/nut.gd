c7b21aa9604a28b1f24773fa0109563df6041679
the-alex-g <alex.gestwicki@gmail.com>
2024-01-28 08:14:47 -0500

    Add nut spin

--- project/player/nut/nut.gd
+++ project/player/nut/nut.gd
@@ -7,16 +7,23 @@ const SPEED := 600.0
 ## This is used to determine when the nut becomes "active" for hitting other things
 var shooter : Node2D
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var _rotations_per_second := randf_range(1.0, 4.0) * (-1 if randi() % 2 == 0 else 1)
 var direction := Vector2.ZERO :
 	set(value):
 		direction = value
 		velocity = direction * SPEED
 
 
+func _ready()->void:
+	rotation = randf() * TAU
+
+
 func _physics_process(delta:float)->void:
 	if is_on_floor():
 		velocity.x = 0
 		set_collision_layer_value(2, false)
+	else:
+		rotation += _rotations_per_second * TAU * delta
 	
 	velocity.y += gravity * delta
 	
